1571ffe6b9c0f0ff75458706d56704ff110b57fe
Rogojan George <rogojang77@gmail.com>
2022-11-09 13:41:24 +0200

    update minesweeper

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-const rows = 10;
-const columns = 10;
+let gameBoard;
+let cell;
+let boardSize = 10;
 
-window.onload = function() {
-    const table = document.getElementById("gameBoard");
-    for (let i = 0; i < rows; ++i) {
-        const row = document.createElement("tr");
-        for (let i = 0; i < columns; ++i) {
-            let cell = document.createElement("td");
-            row.appendChild(cell);
+window.onload = (event) => {
+    createBoard()
+    setInterval(timer, 1000);
+}
+
+function createBoard() {
+    gameBoard = document.getElementById("gameBoard");
+    for (let i = 0; i < boardSize; ++i) {
+        for (let j = 0; j < boardSize; ++j) {
+            cell = document.createElement("tr");
+            cell.className = "cell";
+            cell.id = [i, j];
+            cell.addEventListener("mousedown",  event => {
+                if (event.button == 0) {
+                    showList.push([i, j]);
+                    showElements();
+                } else if (event.button == 2) {
+                    flagMark([i, j]);
+                }
+            }, false);
+            gameBoard.appendChild(cell);
         }
-        table.appendChild(row);
     }
 }
 
+let seconds = 0;
 
+function timer() {
+    seconds += 1
+    document.getElementById('timer').innerHTML = new Date(seconds * 1000).toISOString().slice(14, 19) + " ⏱";
+}
