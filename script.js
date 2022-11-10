2ee3123b064fc1dcd7a9b4394f92672881145ae4
Rogojan George <rogojang77@gmail.com>
2022-11-10 19:47:49 +0200

    update minesweeper

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 let gameBoard;
 let cell;
 let boardSize = 10;
-let bombs = boardSize;
+let mines = boardSize;
+let mineCounter = boardSize;
+const counterDisplay = document.getElementById("minesCounter");
 
 window.onload = () => {
     createBoard()
-    generateBombs();
+    generateMines();
     setInterval(timer, 1000);
 }
 
 function createBoard() {
     gameBoard = document.getElementById("gameBoard");
+    gameBoard.addEventListener('contextmenu', (ev) => {
+        ev.preventDefault();
+    });
     for (let i = 0; i < boardSize; ++i) {
         for (let j = 0; j < boardSize; ++j) {
             cell = document.createElement("tr");
-            cell.className = "cell";
             cell.id = [i, j];
+            cell.onclick = 
+                function() {
+                    clickOnCell(this);
+                };
+            cell.onmouseup = 
+                function() { 
+                    if(this.className != "clicked" && this.className != "mine") {
+                        this.className = "flag";
+                    }
+                };
             gameBoard.appendChild(cell);
         }
     }
@@ -28,13 +42,37 @@ function timer() {
     document.getElementById('timer').innerHTML = new Date(seconds * 1000).toISOString().slice(14, 19) + " ⏱";
 }
 
-function generateBombs() {
-    let bomb;
+function generateMines() {
+    let mine;
     do {
         row = Math.floor(Math.random() * boardSize);
         column = Math.floor(Math.random() * boardSize);
-        bomb = document.getElementById([row, column]);
-        bomb.innerHTML = "💣";
-        --bombs;
-    } while (bombs > 0);
+        mine = document.getElementById([row, column]);
+        mine.setAttribute("mine", "true");
+        --mines;
+    } while (mines > 0);
+}
+
+function selectMines() {
+    for (let i = 0; i < boardSize; ++i) {
+        for (let j = 0; j < boardSize; ++j) {
+            let selectedMines = document.getElementById([i, j]);       
+            if (selectedMines.getAttribute("mine")) {
+                selectedMines.className = "mine";
+            }
+        }    
+    }
+}
+
+function fillCells() {
+    
+}
+
+function clickOnCell(cell) {
+    if (cell.getAttribute("mine")) {
+        selectMines();
+    } else {
+        cell.className = "clicked";
+        fillCells();
+    }
 }
